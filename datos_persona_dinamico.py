be5508b95d3469bc7af20a48715116af9b3f9423
chikarcas <ferromvel@ciencias.unam.mx>
2025-07-02 01:32:41 -0600

    Se modificó el programa datos_persona_dinamico.py para contemplar todos los casos posibles seleccionados por el usuario.

--- datos_persona_dinamico.py
+++ datos_persona_dinamico.py
@@ -7,18 +7,17 @@ persona = {}
 # Creamos la variable que almacenara el valor para seleccionar si se crea una entrada, o no.
 cond = input("¿Quieres agregar un dato sobre la persona? (Sí = 1, No = Cualquier otro valor): ")
 
-# Creamos un conidicional "Sí" para 
-if cond == "1":
-    while cond == "1":
+while cond != "0":
+    if cond == "1":
         key = input("Ingresa el tipo de dato a solicitar: ")
         value = input("Ingresa el valor del dato solicitado: ")
         persona[key] = value
         cond = input("¿Quieres agregar un dato sobre la persona? (Sí = 1, No = 0): ")
-        if cond == "1":
-            continue
-        elif cond == "0":
-            print(persona)
-        else:
+    elif cond == "0":
+        print(persona)
+    else:
+        while cond != "1" and cond != "0":
             cond = input("Introduce un valor valido (Sí = 1, No = 0): ")
-else:
-    print(persona)
+
+print("El programa ha fiinalizado, el diccionario generado es:")
+print(persona)
