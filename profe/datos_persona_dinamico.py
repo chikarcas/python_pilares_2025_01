982970962cfe18de9a27d3d7ad8918ed828f9d8d
Fernando Romero Velázquez <ferromvel@gmail.com>
2025-07-02 11:09:53 -0600

    Se elimino una línea vacia nuevamente.

--- profe/datos_persona_dinamico.py
+++ profe/datos_persona_dinamico.py
@@ -7,7 +7,6 @@ persona = {}
 # Creamos la variable que almacenara el valor para seleccionar si se crea una entrada, o no.
 cond = input("¿Quieres agregar un dato sobre la persona? (Sí = 1, No = Cualquier otro valor): ")
 
-
 while cond != "0":
     if cond == "1":
         key = input("Ingresa el tipo de dato a solicitar: ")
