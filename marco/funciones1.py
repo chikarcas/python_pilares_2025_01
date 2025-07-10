4fbe4b44e663844ff5d921592d3aa059b7c8cdc8
marco <masf58.suarez@gmail.com>
2025-07-10 14:12:02 -0600

    subimos los cambios de la clase de funciones

---
+++ marco/funciones1.py
@@ -0,0 +1,15 @@
+def saludo(n):
+    """Esta función imprime un mensaje saludando a la persona que se escriba como argumento de la función."""
+    print("saludo " + n)
+    return
+
+def suma(a, b):
+    """este funcion nos debuelve el resultado de dos numeros dados"""
+    print(a + b)
+    return
+
+def desc(precio, descuento):
+    """esta funcion nos calcula el precio con descuento de un articulo"""
+    print(precio - (precio * (descuento / 100)))
+    return
+
