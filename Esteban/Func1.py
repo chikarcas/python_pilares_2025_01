b6bea0ff129a30032e2bdd9976ff3c0d10956910
Steve <joseestebanespi3@gmail.com>
2025-07-23 13:27:15 -0600

    Se hizo una funcion para detectar si es numero primo o no

--- Esteban/Func1.py
+++ Esteban/Func1.py
@@ -1,14 +1,26 @@
 def saludo(n):
-    """Esta funcion imprime un saludo a la persona que se escrba como argumento"""
+    """Esta funcion imprime un saludo a la persona que se escrba como argumento."""
     print("Hola "+ n)
     return
 def suma(a, b):
-    '''Este programa nos devuelve el resultado de la suma impresa de dos numeros dados '''
+    '''Este programa nos devuelve el resultado de la suma impresa de dos numeros dados. '''
     print(a+b)
     return
 def descuento(pr, des):
-    "Este programa  te da el precio con descuento de un producto"
+    """Este programa  te da el precio con descuento de un producto."""
     print("Total con descuento: "+ str(pr-pr*des/100))
     print('Total descontado: '+ str(pr*des/100))
     return
-descuento(100, 20)
+def tipo(x):
+    '''Este programa te devuelve el tipo de variable que es.'''
+    print('El tipo de variable es: '+ str(type(x)))
+    return
+def numprim(y):
+    '''Este programa detacta si un numero es primo o no.'''
+    for i in range(2, y):
+        mod=y%i
+        if mod !=0:
+            continue
+        else:
+            return print('No es un numero primo.')
+    return print('Es un numero primo.')
--- Esteban/Prueba de funciones.py
+++ Esteban/Prueba de funciones.py
@@ -1,2 +1,10 @@
 import Func1 as fn
 fn.saludo("Esteban")
+fn.descuento(1000, 35)
+fn.tipo(0)
+fn.numprim(97)
+'''a=13
+b=6
+c=5
+print(13%2)
+print(a%c)'''
